c0a8beae6611bf59af086fb2677b80bb550c043c
Vignesh-dev07 <79962157+Vignesh-dev07@users.noreply.github.com>
2021-03-09 23:18:45 +0530

    Add files via upload

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,12 @@
+import random
 #def main():
-  #print("Keep it logically awesome.")
+#print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+f = open("quotes.txt")
+quotes = f.readlines()
+f.close()
+rnd=random.randint(0,len(quotes))
+print(quotes[rnd])
 
-  print(quotes)
-'''
-if __name__== "__main__":
-  #main()
-'''
+#if __name__== "__main__":
+ # main()
